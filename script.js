1d4eea70a9dafc9079b97a913a4ef09022de9632
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-09-26 12:28:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
 //your code here
-var itemName = document.getElementById("item-name-input");
-var itemQuantity = document.getElementById("item-qty-input");
-var itemPrice = document.getElementById("item-price-input");
- 
-var tbody = document.querySelector("tbody");
- 
-var grandTotal = document.getElementById("total");
-var grand_total = 0;
- 
-var addItem = document.querySelector("#add");
- 
-addItem.addEventListener("click", (event) => {
-  event.preventDefault();
- 
-  let newRow = document.createElement("tr");
- 
-  let name = document.createElement("td");
-  name.innerText = itemName.value;
-  name.classList.add("item");
-  newRow.append(name);
- 
-  let price = document.createElement("td");
-  price.innerText = itemPrice.value;
-  newRow.append(price);
-  price.classList.add("price");
- 
-  let quantity = document.createElement("td");
-  quantity.innerText = itemQuantity.value;
-  newRow.append(quantity);
- 
-  if (itemName.value && itemQuantity.value && itemPrice.value) {
+
+// itemName, itemQuantity ,itemPrice , tbody ,addItem 
+
+let itemName = document.getElementById('item-name-input');
+let itemQuantity = document.getElementById('item-qty-input');
+let itemPrice = document.getElementById('item-price-input');
+
+let tbody = document.querySelector('tbody');
+let addItem = document.querySelector('#add');
+
+let GrandTotal = document.getElementById('total');
+let grand_total = 0;
+
+addItem.addEventListener('click', (event) => {
+
+let newRow = document.createElement('tr');
+
+	let name = document.createElement('td')
+     name.innerText = itemName.value;
+	 name.classList.add('item');
+	 newRow.append(name)
+
+	let quantity = document.createElement('td')
+     quantity.innerText = itemQuantity.value;
+     newRow.append(quantity)
+
+	let price = document.createElement('td')
+     price.innerText = itemPrice.value;
+	 price.classList.add('price');
+	 newRow.append(price)
+
+     if (itemName.value && itemQuantity.value && itemPrice.value) {
     let total = document.createElement("td");
     total.innerText = itemPrice.value * itemQuantity.value;
     newRow.append(total);
- 
-    tbody.append(newRow);
- 
-    grand_total += Number(total.innerText);
-    grandTotal.innerHTML = "Grand Total: ₹ " + grand_total;
-  }
- 
-  itemName.value = "";
-  itemQuantity.value = "";
-  itemPrice.value = "";
-});
- 
+
+     tbody.append(newRow);
+
+	 grand_total = Number(total.innerText);
+
+	GrandTotal.innerHTML = 'Grand Total: ₹' + grand_total ;
+
+	itemName.value = '';
+	itemQuantity.value = '';
+	itemPrice.value = '';
+})
+
+
+
+		
